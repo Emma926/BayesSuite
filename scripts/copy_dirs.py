7925cd36cfb875c4d9c954fc1700ce27693cf29a
Emma926 <emmawong926@gmail.com>
2019-03-16 19:35:43 -0400

    scripts for stancon, not tested yet

---
+++ scripts/copy_dirs.py
@@ -0,0 +1,25 @@
+'''
+This script copies the selected workloads from stancon/ to BayesSuite/.
+
+Yu (Emma) Wang
+'''
+
+import os
+
+wl_paths = {
+  '0_12cities': 'stancon/2017/Contributed-Talks/01_auerbach',
+  '1_ad': 'stancon/2017/Contributed-Talks/03_lei',
+  '2_ode': 'stancon/2017/Contributed-Talks/05_margossian',
+  '3_memory': 'stancon/2017/Contributed-Talks/07_nicenboim',
+  '4_votes': 'stancon/2017/Contributed-Talks/08_trangucci',
+  '5_tickets': 'stancon/2018/Contributed-Talks/01_auerbach',
+  '6_disease': 'stancon/2018/Contributed-Talks/02_pourzanjani',
+  '7_racial': 'stancon/2018/Contributed-Talks/11_simoiu',
+}
+
+root = os.path.realpath('..')
+
+for wl, path in wl_paths.iteritems():
+    if os.path.isdir(root, 'BayesSuite', wl):
+        continue
+    os.system('cp -r ' + os.path.join(root, path) + ' ' + os.path.join(root, 'BayesSuite', wl)) 
---
+++ scripts/generate_run_scripts.sh
@@ -0,0 +1,65 @@
+cd ../BayesSuite/0_12cities
+
+#rstan_options(auto_write = TRUE)
+
+
+sed -n '11,20 p' twelve_cities.Rmd > run.R
+sed -n '190,208 p' twelve_cities.Rmd >> run.R
+sed -n '369,413 p' twelve_cities.Rmd >> run.R
+
+cd ../1_ad
+sed -n '332,377 p' ad_attribution.Rmd > run.R
+sed -n '381,388 p' ad_attribution.Rmd >> run.R
+
+cd ../2_ode
+sed -n '18,18 p' ODEStan.Rmd > run.R
+sed -n '20,21 p' ODEStan.Rmd >> run.R
+sed -n '692,697 p' ODEStan.Rmd >> run.R
+sed -n '705,720 p' ODEStan.Rmd >> run.R
+sed -n '726,760 p' ODEStan.Rmd >> run.R
+
+cd ../3_memory
+sed -n '350,350 p' retrieval_models.Rmd > run.R
+sed -n '353,361 p' retrieval_models.Rmd >> run.R
+sed -n '834,848 p' retrieval_models.Rmd >> run.R
+sed -n '859,860' retrieval_models.Rmd >> run.R
+
+cd ../4_votes
+sed -n '11,11 p' hierarchical_GPs_in_stan.Rmd > run.R
+sed -n '13,13 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '16,16 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '781,782 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '793,793 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '810,832 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '843,853 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '944,945 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '948,948 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '955,967 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '1147,1147 p' hierarchical_GPs_in_stan.Rmd >> run.R
+sed -n '1151,1152 p' hierarchical_GPs_in_stan.Rmd >> run.R
+
+cd ../5_tickets
+sed -n '104,124 p' auerbach_jonathan.Rmd > run.R
+sed -n '128,143 p' auerbach_jonathan.Rmd >> run.R
+sed -n '162,240 p' auerbach_jonathan.Rmd >> run.R
+sed -n '257,274 p' auerbach_jonathan.Rmd >> run.R
+sed -n '283,304 p' auerbach_jonathan.Rmd >> run.R
+sed -n '314,352 p' auerbach_jonathan.Rmd >> run.R
+sed -n '380,389 p' auerbach_jonathan.Rmd >> run.R
+sed -n '415,351 p' auerbach_jonathan.Rmd >> run.R
+sed -n '633,640 p' auerbach_jonathan.Rmd >> run.R
+
+cd ../6_disease
+sed -n '24,29 p' ad_progression.Rmd > run.R
+sed -n '46,50 p' ad_progression.Rmd >> run.R
+sed -n '55,62 p' ad_progression.Rmd >> run.R
+sed -n '106,114 p' ad_progression.Rmd >> run.R
+sed -n '321,322 p' ad_progression.Rmd >> run.R
+
+cd ../7_racial
+sed -n '10,23 p' threshold-test.Rmd > run.R
+sed -n '155,163 p' threshold-test.Rmd >> run.R
+sed -n '369,389 p' threshold-test.Rmd >> run.R
+
+#cd ../8_butterfly
+#cd ../9_survival
